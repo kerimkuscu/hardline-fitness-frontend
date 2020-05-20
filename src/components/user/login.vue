db43d4a6724fc328e358387a3705dbdae25997ed
kerimkuscu <kerimkuscu95@gmail.com>
2020-05-21 01:19:23 +0300

    refactor: change style and add method for login page

--- src/components/user/login.vue
+++ src/components/user/login.vue
@@ -1,30 +1,42 @@
 <template>
   <div class="col-12">
     <div class="row">
-      <div class="col-6">
+      <div class="col-4">
         <div style="margin-top: 270px">
           <form @submit.prevent="submit">
             <div class="form-group row">
               <label for="email" class="col-1"><i class="fas fa-user" /></label>
               <div class="col-10">
-                <input id="email" type="email" class="form-control form-control-sm" aria-describedby="emailHelp">
+                <input
+                  id="email"
+                  v-model="form.email"
+                  type="email"
+                  class="form-control form-control-sm"
+                  aria-describedby="emailHelp"
+                  placeholder="Email"
+                >
               </div>
             </div>
             <div class="form-group row">
               <label for="password" class="col-1"><i class="fas fa-lock" /></label>
               <div class="col-10">
-                <input id="password" type="password" class="form-control form-control-sm">
+                <input id="password" v-model="form.password" type="password" class="form-control form-control-sm" placeholder="Password">
               </div>
             </div>
 
             <button :disabled="disabled" type="submit" class="btn btn-primary offset-5">
-              Login
+              <span v-if="disabled">
+                <i class="fas fa-spinner fa-pulse" /> Login...
+              </span>
+              <span v-else>
+                Login
+              </span>
             </button>
           </form>
         </div>
       </div>
-      <div class="col-6">
-        <img style="height: 619px; width: 625px" src="../../assets/hulk.jpg" alt="login">
+      <div class="col-8">
+        <admin-panel-image />
       </div>
     </div>
   </div>
@@ -32,9 +44,14 @@
 
 <script>
     import Form from 'form-backend-validation';
+    import AdminPanelImage from './admin-panel-image';
 
     export default {
         name: 'Login',
+
+        components: {
+            AdminPanelImage
+        },
 
         data: () => ({
             form: new Form({
@@ -44,20 +61,22 @@
 
             disabled: false,
         }),
-        
+
         methods: {
             async submit() {
-              this.disabled = true;
+                this.disabled = true;
 
                 try {
-                  const response = await this.form.post('');
-                  window.toastr.success(response.status);
+                    const response = await this.form.post('http://localhost:8000/login');
+                    window.toastr.success(this.$i18n.t('entry_successful'));
+                    window.eventHub.$emit('user-token', response);
+                    window.location.href = '/';
                 } catch (error) {
-                  if (error.response.status !== 422) {
-                    window.toastr.error(error.response.status);
-                  }
-                }finally {
-                  this.disabled = false;
+                    if (error.response) {
+                        window.toastr.error(error.response.data.error);
+                    }
+                } finally {
+                    this.disabled = false;
                 }
             }
         }
