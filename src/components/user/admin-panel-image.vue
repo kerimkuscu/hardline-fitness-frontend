fa8ec55890404f8839ca0709fa80d3ba3f5776d4
kerimkuscu <kerimkuscu95@gmail.com>
2020-05-21 01:19:09 +0300

    refactor: add admin panel image

---
+++ src/components/user/admin-panel-image.vue
@@ -0,0 +1,9 @@
+<template>
+  <img class="float-right" style="height: 619px;width: 850px; margin-right: -15px;" src="../../assets/hulk.jpg" alt="login">
+</template>
+
+<script>
+    export default {
+        name: 'AdminPanelImage',
+    }
+</script>
