310104013ac65548bff360f0e8888d003776175c
kerimkuscu <kerimkuscu95@gmail.com>
2020-05-23 18:58:09 +0300

    refactor: add a exercise route

--- src/components/router/routes.js
+++ src/components/router/routes.js
@@ -1,6 +1,7 @@
 import Home from '../home/home';
 import Login from '../user/login';
 import Register from '../user/register';
+import Exercises from '../exercises/exercises';
 import NotFound from '../not-found';
 
 export default [
@@ -20,6 +21,12 @@ export default [
         path: '/register',
         name: 'register',
         component: Register,
+    },
+
+    {
+        path: '/exercises',
+        name: 'exercises',
+        component: Exercises
     },
 
     { path: '*', component: NotFound }
